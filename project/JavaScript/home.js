ba50ed49bcd3c0168b6ca3f905b4f49a6a6218d5
Julian Strasser <j.strasser@students.htl-leonding.ac.at>
2025-03-19 10:34:22 +0100

    Settings Unterseite begonnen

---
+++ project/JavaScript/home.js
@@ -0,0 +1,12 @@
+function printSettings() {
+ let settings = `
+    <div id="settings">
+        <div class="settOp">
+            <div class="textBox"></div>
+        </div>
+
+    
+    
+    </div>
+ `    
+}
