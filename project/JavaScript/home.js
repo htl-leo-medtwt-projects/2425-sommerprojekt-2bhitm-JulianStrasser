930be7f8bdc2e9d05dfaddf9094232deead70cbc
Julian Strasser <j.strasser@students.htl-leonding.ac.at>
2025-03-19 12:22:07 +0100

    Home Hover Effekt erstellt, JS angefangen

--- project/JavaScript/home.js
+++ project/JavaScript/home.js
@@ -1,12 +1,54 @@
+let outputHome = document.getElementById("outputHome");
+
+disableScrolling();
+
+function disableScrolling() {
+    document.body.style.overflow = 'hidden';
+}
+
+function enableScrolling() {
+    document.body.style.overflow = 'auto';
+}
+
 function printSettings() {
  let settings = `
     <div id="settings">
         <div class="settOp">
-            <div class="textBox"></div>
+            <div class="textBox">Language</div>
+
+            <div class="settingsSelection">
+                <img class="arrowLeft" src="img/arrowLeft.png" class="arrow">
+                <p>English</p>
+                <img class="arrowRight" src="img/arrowRight.png" class="arrow">
+            </div>
+            
         </div>
 
+        <div class="settOp">
+            <div class="textBox">Fullscreen</div>
+
+            <div class="settingsSelection">
+                <img class="arrowLeft" src="img/arrowLeft.png" class="arrow">
+                <p>Off</p>
+                <img class="arrowRight" src="img/arrowRight.png" class="arrow">
+            </div>
+            
+        </div>
+
+        <div class="settOp">
+            <div class="textBox">Language</div>
+
+            <div class="settingsSelection">
+                <img class="arrowLeft" src="img/arrowLeft.png" class="arrow">
+                <p>English</p>
+                <img class="arrowRight" src="img/arrowRight.png" class="arrow">
+            </div>
+            
+        </div>
     
     
     </div>
- `    
+ `;
+
+    outputHome.innerHTML = settings;
 }
